eb53fd023f8545822b1689b550304155e905660e
Aarna <aarnaburji@gmail.com>
2025-07-28 10:21:43 -0700

    first

---
+++ ToDoList/NewToDoList.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoList.swift
+//  ToDoList
+//
+//  Created by Aarna Burji on 7/28/25.
+//
+
+import SwiftUI
+
+struct NewToDoList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoList()
+}
